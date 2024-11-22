ff0396f8f8634556b58cddbaac22bfcd3a883083
halftone-dev <halftonedev@gmail.com>
2024-11-22 14:13:35 +0000

    Add faucet button back in

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 // @refresh reset
-import { Balance } from "../Balance";
-import { AddressInfoDropdown } from "./AddressInfoDropdown";
-import { AddressQRCodeModal } from "./AddressQRCodeModal";
-import { WrongNetworkDropdown } from "./WrongNetworkDropdown";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Address } from "viem";
-import { getBlockExplorerAddressLink } from "~~/utils/scaffold-eth";
-import { FhenixPermitV2Dropdown } from "../fhenix/PermitV2/FhenixPermitV2Dropdown";
+// import { Balance } from "../Balance";
+// import { AddressInfoDropdown } from "./AddressInfoDropdown";
+// import { AddressQRCodeModal } from "./AddressQRCodeModal";
+// import { WrongNetworkDropdown } from "./WrongNetworkDropdown";
+// import { ConnectButton } from "@rainbow-me/rainbowkit";
+// import { Address } from "viem";
+// import { getBlockExplorerAddressLink } from "~~/utils/scaffold-eth";
+// import { FhenixPermitV2Dropdown } from "../fhenix/PermitV2/FhenixPermitV2Dropdown";
 import { FaucetButton } from "../FaucetButton";
 import { useUser, useAuthModal, useSignerStatus, useLogout } from "@account-kit/react";
 
@@ -21,26 +21,28 @@ export const AccountKitCustomConnectButton = () => {
   const signerStatus = useSignerStatus();
   const { logout } = useLogout();
 
-  console.log({ signerStatus, user });
-
-  if (signerStatus.isInitializing) return <div className="btn btn-disabled">Loading...</div>;
-
-  if (user == null || signerStatus.isDisconnected)
-    return (
-      <div className="btn" onClick={openAuthModal}>
-        LOGIN
-      </div>
-    );
-
   return (
-    <div
-      className="btn"
-      onClick={() => {
-        console.log("logout");
-        logout();
-      }}
-    >
-      {user.email ?? "anon"}
+    <div className="flex flex-row gap-4">
+      <FaucetButton />
+      {signerStatus.isInitializing && <div className="btn btn-disabled">Loading...</div>}
+
+      {(user == null || signerStatus.isDisconnected) && (
+        <div className="btn" onClick={openAuthModal}>
+          LOGIN
+        </div>
+      )}
+
+      {user != null && signerStatus.isConnected && (
+        <div
+          className="btn"
+          onClick={() => {
+            console.log("logout");
+            logout();
+          }}
+        >
+          {user.email ?? "anon"}
+        </div>
+      )}
     </div>
   );
 };
