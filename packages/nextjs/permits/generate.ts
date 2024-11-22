8c1a94249af735dff39835833be9efe6a5fbab9c
halftone-dev <halftonedev@gmail.com>
2024-11-22 17:19:28 +0000

    Add permitV2 stuff from active fhenix.js PR

---
+++ packages/nextjs/permits/generate.ts
@@ -0,0 +1,62 @@
+import { EIP712Message } from "fhenixjs";
+import { PermissionV2 } from "./types";
+import { EIP712Types } from "fhenixjs";
+
+const PermitV2SignatureAllFields = [
+  { name: "issuer", type: "address" },
+  { name: "expiration", type: "uint64" },
+  { name: "contracts", type: "address[]" },
+  { name: "projects", type: "string[]" },
+  { name: "recipient", type: "address" },
+  { name: "validatorId", type: "uint256" },
+  { name: "validatorContract", type: "address" },
+  { name: "sealingKey", type: "bytes32" },
+  { name: "issuerSignature", type: "bytes" },
+] as const;
+type PermitV2SignatureFieldOption = (typeof PermitV2SignatureAllFields)[number]["name"];
+
+export const SignatureTypes = {
+  PermissionedV2IssuerSelf: [
+    "issuer",
+    "expiration",
+    "contracts",
+    "projects",
+    "recipient",
+    "validatorId",
+    "validatorContract",
+    "sealingKey",
+  ] satisfies PermitV2SignatureFieldOption[],
+  PermissionedV2IssuerShared: [
+    "issuer",
+    "expiration",
+    "contracts",
+    "projects",
+    "recipient",
+    "validatorId",
+    "validatorContract",
+  ] satisfies PermitV2SignatureFieldOption[],
+  PermissionedV2Receiver: ["sealingKey", "issuerSignature"] satisfies PermitV2SignatureFieldOption[],
+} as const;
+type SignatureIdentifier = keyof typeof SignatureTypes;
+
+export const getSignatureTypesAndMessage = <T extends PermitV2SignatureFieldOption>(
+  typeName: SignatureIdentifier,
+  fields: T[] | readonly T[],
+  values: Pick<PermissionV2, T> & Partial<PermissionV2>,
+): { types: EIP712Types; message: EIP712Message } => {
+  const types = {
+    [typeName]: PermitV2SignatureAllFields.filter(fieldType => fields.includes(fieldType.name as T)),
+  };
+
+  const message: Record<T, string | string[] | number | number[]> = {} as Record<
+    T,
+    string | string[] | number | number[]
+  >;
+  fields.forEach(field => {
+    if (field in values) {
+      message[field] = values[field];
+    }
+  });
+
+  return { types, message: message as EIP712Message };
+};
