b5a26e6950490e22f6b2f63c339ae48033b85979
halftone-dev <halftonedev@gmail.com>
2024-11-24 16:46:15 +0000

    Add minting functionality to smart wallet to force deployment

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,13 +1,53 @@
 "use client";
 
-import { useAccount, useChain } from "@account-kit/react";
+import { useAccount, useChain, useSendUserOperation, useSmartAccountClient } from "@account-kit/react";
 import type { NextPage } from "next";
+import { encodeFunctionData } from "viem";
 import { ConfidentialityRatioHeader } from "~~/components/ConfidentialityRatioHeader";
 import { PortfolioTotalHeader } from "~~/components/PortfolioTotalHeader";
 import { SortedTokens } from "~~/components/SortedTokens";
+import { useDeployedContractInfo } from "~~/hooks/scaffold-eth";
 import { PermitV2 } from "~~/permits/permitV2";
 import { setActivePermitHash, setPermit } from "~~/permits/store";
 import { AbstractSigner } from "~~/permits/types";
+
+// If the lightAccount isn't deployed, send 0.01 ETH to itself to force a deployment
+const DeployLightAccountButton = () => {
+  const { client } = useSmartAccountClient({ type: "LightAccount" });
+  const { data: usdcContract } = useDeployedContractInfo("USDC");
+
+  const { sendUserOperation, isSendingUserOperation } = useSendUserOperation({
+    client,
+    waitForTxn: true,
+    onSuccess: ({ hash, request }) => {
+      console.log("Success deploying smart wallet", hash, request);
+    },
+    onError: error => {
+      console.log("Error deploying smart wallet", error);
+    },
+  });
+
+  const mintUsdc = () => {
+    if (client == null) return;
+    if (usdcContract == null) return;
+    sendUserOperation({
+      uo: {
+        target: usdcContract.address,
+        data: encodeFunctionData({
+          abi: usdcContract.abi,
+          functionName: "mint",
+          args: [client.getAddress(), 10_000_000n],
+        }),
+      },
+    });
+  };
+
+  return (
+    <div className="btn" onClick={() => mintUsdc()}>
+      {isSendingUserOperation ? "Deploying..." : "Force Smart Wallet Deployment"}
+    </div>
+  );
+};
 
 const CreatePermitV2Button = () => {
   const { chain } = useChain();
@@ -50,6 +90,7 @@ const CreatePermitV2Button = () => {
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col max-w-[975px] gap-12 mx-auto p-8 pt-32 w-full">
+      <DeployLightAccountButton />
       <CreatePermitV2Button />
 
       <div className="flex flex-col gap-6 justify-center items-center">
