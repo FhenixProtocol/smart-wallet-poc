4ef18efc94f0c89ec016ecf5873697a7343e7fc7
halftone-dev <halftonedev@gmail.com>
2024-12-04 12:57:07 +0000

    Get alchemyApiKey from different place

--- packages/nextjs/account-kit.config.ts
+++ packages/nextjs/account-kit.config.ts
@@ -1,5 +1,6 @@
 import { AlchemyAccountsUIConfig, cookieStorage, createConfig as createAccountKitConfig } from "@account-kit/react";
 import { alchemy, sepolia } from "@account-kit/infra";
+import scaffoldConfig from "./scaffold.config";
 // TODO: Re-introduce fhenix nitrogen once it is added to alchemy account-kit
 
 const accountKitUiConfig: AlchemyAccountsUIConfig = {
@@ -15,7 +16,7 @@ const accountKitUiConfig: AlchemyAccountsUIConfig = {
         },
       ],
     ],
-    addPasskeyOnSignup: false,
+    addPasskeyOnSignup: true,
   },
 };
 
@@ -23,7 +24,7 @@ export const accountKitConfig = createAccountKitConfig(
   {
     // if you don't want to leak api keys, you can proxy to a backend and set the rpcUrl instead here
     // get this from the app config you create at https://dashboard.alchemy.com/accounts?utm_source=demo_alchemy_com&utm_medium=referral&utm_campaign=demo_to_dashboard
-    transport: alchemy({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string }),
+    transport: alchemy({ apiKey: scaffoldConfig.alchemyApiKey }),
     chain: sepolia, // defineAlchemyChain({ chain: fhenixNitrogen, rpcBaseUrl: "https://eth-sepolia.g.alchemy.com/v2" }),
     ssr: true, // set to false if you're not using server-side rendering
     storage: cookieStorage, // persist the account state using cookies (read more here: https://accountkit.alchemy.com/react/ssr#persisting-the-account-state)
